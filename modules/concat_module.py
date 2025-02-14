df56107c8ac3cb81d306af0b5492f5e7adbe5ba3
Reina234 <zhengreina@gmail.com>
2025-02-14 07:30:32 +0000

    fix concat

--- modules/concat_module.py
+++ modules/concat_module.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 # Inside FusionModule
 class ConcatModule(nn.Module):
     def __init__(self, global_dim: int, fg_dim: int, out_dim: int):
-        super(FusionModule, self).__init__()
+        super(ConcatModule, self).__init__()
         self.fc = nn.Linear(
             global_dim + fg_dim, out_dim
         )  # Simple concatenation-based fusion
